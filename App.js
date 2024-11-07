da2ae9ffed9948abec180d5a5218909a6f8c20a2
DoomWave <acunanajair9@gmail.com>
2024-11-06 19:14:40 -0800

    I think it's a good start.

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { s } from "./App.style"
+import { View } from "react-native";
+import { s } from "./App.style";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [x, setX] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      setX((x) => x + 1);
+    }, 25);
+  }, []);
+
   return (
     <View style={s.root}>
-    <View style={s.square}/>
+      <View style={[s.square, { left: x }]} />
     </View>
-  )
+  );
 }
