977a7df86b455c604fca0eeddb4cf979d848dfb0
DoomWave <acunanajair9@gmail.com>
2024-11-08 17:11:59 -0800

    many fixes, but it worth it.

---
+++ App.jsx
@@ -0,0 +1,64 @@
+import { LogBox, View } from "react-native";
+import {
+  GestureHandlerRootView,
+  PanGestureHandler,
+} from "react-native-gesture-handler";
+import Animated, {
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
+import { s, CIRCLE_RADIUS, CIRCLE_PERIMETER, SQUARE_SIZE } from "./App.style";
+
+LogBox.ignoreLogs(["No native splash"]);
+
+export default function App() {
+  const squareAnimTranslateX = useSharedValue(0);
+  const squareAnimTranslateY = useSharedValue(0);
+
+  const gestureHandler = useAnimatedGestureHandler({
+    onStart: (_, ctx) => {
+      ctx.initialPosX = squareAnimTranslateX.value;
+      ctx.initialPosY = squareAnimTranslateY.value;
+    },
+    onActive: (e, ctx) => {
+      squareAnimTranslateX.value = ctx.initialPosX + e.translationX;
+      squareAnimTranslateY.value = ctx.initialPosY + e.translationY;
+    },
+    onEnd: (e, ctx) => {
+      console.log("onEnd");
+      const distanceFromCenter = Math.sqrt(
+        squareAnimTranslateX.value ** 2 + squareAnimTranslateY.value ** 2
+      );
+      if (distanceFromCenter < CIRCLE_RADIUS) {
+        squareAnimTranslateX.value = withSpring(0);
+        squareAnimTranslateY.value = withSpring(0);
+      }
+    },
+  });
+
+  const squareAnimStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: squareAnimTranslateX.value,
+        },
+        {
+          translateY: squareAnimTranslateY.value,
+        },
+      ],
+    };
+  });
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View style={s.root}>
+        <View style={s.circle}>
+          <PanGestureHandler onGestureEvent={gestureHandler}>
+            <Animated.View style={[s.square, squareAnimStyle]} />
+          </PanGestureHandler>
+        </View>
+      </View>
+    </GestureHandlerRootView>
+  );
+}
--- App.style.js
+++ App.style.js
@@ -1,13 +1,26 @@
 import { StyleSheet } from "react-native";
-
+export const SQUARE_SIZE = 100;
+export const CIRCLE_PERIMETER = 350
+export const CIRCLE_RADIUS = CIRCLE_PERIMETER/2
 export const s = StyleSheet.create({
     root: {
         flex: 1, 
         justifyContent: "center", 
+        alignItems: 'center'
     },
     square: {
-        height: 100,
-        width: 100,
+        borderRadius: 15,
+        height: SQUARE_SIZE,
+        width: SQUARE_SIZE,
         backgroundColor: "orange",
+    },
+    circle: {
+        borderWidth: 5,
+        borderColor: "orange",
+        height: CIRCLE_PERIMETER,
+        width: CIRCLE_PERIMETER,
+        borderRadius: CIRCLE_RADIUS,
+        justifyContent: "center",
+        alignItems: "center"
     }
 });
