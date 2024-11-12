c65d9a08b68967b41bee32f2ff6b01a3a705d5e1
DoomWave <acunanajair9@gmail.com>
2024-11-11 18:43:41 -0800

    adding more stuff.

--- App.style.js
+++ App.style.js
@@ -1,25 +1,6 @@
 import { StyleSheet } from "react-native";
-export const SQUARE_SIZE = 100;
-export const CIRCLE_PERIMETER = 350
-export const CIRCLE_RADIUS = CIRCLE_PERIMETER/2
+
 export const s = StyleSheet.create({
-    root: {
-        flex: 1, 
-        justifyContent: "center", 
-    },
-    square: {
-        borderRadius: 15,
-        height: SQUARE_SIZE,
-        width: SQUARE_SIZE,
-        backgroundColor: "orange",
-    },
-    circle: {
-        borderWidth: 5,
-        borderColor: "orange",
-        height: CIRCLE_PERIMETER,
-        width: CIRCLE_PERIMETER,
-        borderRadius: CIRCLE_RADIUS,
-        justifyContent: "center",
-        alignItems: "center"
-    }
+
+    
 });
