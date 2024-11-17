3657699890dd0508f92d1a1060a6fb2221322d2f
DoomWave <acunanajair9@gmail.com>
2024-11-16 18:26:14 -0800

    some fixes and a better scrollY

--- App.jsx
+++ App.jsx
@@ -2,6 +2,7 @@ import { LogBox, View, Dimensions } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import {List} from "./components/List/List/List"
 import { s } from "./App.style";
+import { useSharedValue } from "react-native-reanimated";
 LogBox.ignoreLogs(["No native splash"]);
 
 const {width: SCREEN_W, height: SCREEN_H} = Dimensions.get("screen")
